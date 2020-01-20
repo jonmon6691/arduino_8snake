8c4e78f41972f85cee3ef02de5a7dd1eecfba01a
jonmon6691 <jonmon6691@gmail.com>
2020-01-19 18:33:35 -0800

    good basis for snake here data structures seem good, collision detection is working, next step is to implement the deleting of the end of the snake as it moves.

--- arduino_8snake.ino
+++ arduino_8snake.ino
@@ -17,24 +17,25 @@ Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 // super simple rising edge and debounce detector
 // Warning: only works if all 4 button pin numbers are sequential
 uint8_t db[4] = {0};
-void on_button(int pin, int debounce, void (*callback)())
+void on_button(int pin, int debounce, void (*callback)(uint8_t), const uint8_t context)
 {
   if (!digitalRead(pin)) {
     if (!db[pin%4]) {
       db[pin%4] = debounce;
-      callback();
+      callback(context);
     }
   } else if (db[pin%4]) db[pin%4]--;
 }
 
-// state objects
-struct {
-  int x;
-  int y;
-  int color;
-} you;
-
-int current_direction = 0;
+#define index(x, y) ((y)*8 + (x))
+#define x(i) ((i) % 8)
+#define y(i) ((i) / 8)
+int8_t head = index(2,2);
+// index is a pixel in the screen y*8+x
+// The value given by snake[index]-1 is the index of the next snake pixel
+// Sentinal value 0 means no snake part at that pixel
+int8_t snake[8*8];
+uint8_t current_direction = 0;
 
 #define SOUTH (1 << 0)
 #define EAST  (1 << 1)
@@ -42,29 +43,9 @@ int current_direction = 0;
 #define WEST  (1 << 3)
 
 // Returns true if you can go in the requested direction
-bool check(uint8_t direction)
+bool check(int8_t next)
 {
-  switch (direction)
-  {
-  case NORTH:
-    return true;
-    break;
-  
-  case SOUTH:
-    return true;
-    break;
-
-  case EAST:
-    return true;
-    break;
-
-  case WEST:
-    return true;
-    break;
-
-  default:
-    return false;
-  }
+  return snake[next] == -1;
 }
 
 void lose()
@@ -72,29 +53,40 @@ void lose()
   return;
 }
 
+int8_t next_head(uint8_t direction)
+{
+  switch (direction) {
+  case NORTH: return index(x(head), (8 + y(head) - 1) % 8); break;
+  case SOUTH: return index(x(head), (8 + y(head) + 1) % 8); break;
+  case EAST:  return index((8 + x(head) + 1) % 8, y(head)); break;
+  case WEST:  return index((8 + x(head) - 1) % 8, y(head)); break;
+  default:    return -1; break;
+  }
+}
+
+void grow(int8_t next)
+{
+  snake[next] = head;
+  head = next;
+  Serial.println(head);
+}
+
 // Button handlers
-void move_up()
+void move(uint8_t direction)
 {
-  if (!check(NORTH)) lose();
+  int8_t next = next_head(direction);
+  if (!check(next)) {
+    lose();
+  } else {
+    grow(next);
+    current_direction = direction;
+  }
   update_screen();
 }
 
-void move_down()
+void turn(uint8_t direction)
 {
-  if (!check(SOUTH)) lose();
-  update_screen();
-}
-
-void move_left()
-{
-  if (!check(WEST)) lose();
-  update_screen();
-}
-
-void move_right()
-{
-  if (!check(EAST)) lose();
-  update_screen();
+  current_direction = direction;
 }
 
 void update_screen()
@@ -106,8 +98,17 @@ void update_screen()
 
 void draw_snake()
 {
-  ;
+  for (int i = head; ; i = snake[i]) {
+    Serial.print(x(i));Serial.print(" ");
+    Serial.print(y(i));Serial.print(" ");
+    Serial.print(snake[i]);Serial.println(" ");
+    matrix.drawPixel(x(i), y(i), LED_YELLOW);
+    if (snake[i] == -1) break;
+  }
+  Serial.println(" ");
 }
+
+
 
 void setup()
 {
@@ -126,18 +127,20 @@ void setup()
   // My screen is soldered in the "1" direction
   matrix.setRotation(1);
 
-  // Init
-  you.x = you.y = 1; // Start in the upper left corner
-  you.color = LED_GREEN;
-
+  for (int i = 0; i < 8*8; i++) {
+    snake[i] = -1;
+  }
   update_screen();
 }
 
 void loop()
 {
+  if (millis() % 500 == 0) {
+    move(current_direction);
+  }
   // Polling loop
-  on_button(UB, 5, move_up);
-  on_button(DB, 5, move_down);
-  on_button(LB, 5, move_left);
-  on_button(RB, 5, move_right);
+  on_button(UB, 25, turn, NORTH);
+  on_button(DB, 25, turn, SOUTH);
+  on_button(LB, 25, turn, WEST);
+  on_button(RB, 25, turn, EAST);
 }
